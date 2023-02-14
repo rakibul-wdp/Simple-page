349317da2631dcb957176c26a5cde1f17c4a6213
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-14 17:06:40 +0600

    connecti with mongodb

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log("Hello Server");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv").config();
+const routes = require("./routes/route");
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(routes);
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    `${process.env.MONGO_URL}`
+  )
+  .then((data) => {
+    console.log("Database connected");
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log("Error occur in database", err);
+  });
